38482adafe2ec865bce050ae0f79df45b4610cca
lev.kedem@gmail.com <lev.kedem@gmail.com>
2021-11-09 09:39:15 +0200

    9/11-masahim

---
+++ Exercise.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System.Threading;
+
+
+namespace AvTipus2
+{
+    class Exercise
+    {
+        private int time;
+        public bool isGoing;
+
+        public Exercise()
+        {
+            time = 0;
+            isGoing = true;
+        }
+        public String getTimer()
+        {
+
+            int min = this.time / 60;
+            int sec = this.time - min * 60;
+            string s, m;
+            if (min<10)
+            {
+                m = "0" + min;
+            }
+            else
+            {
+                m = "" + min;
+            }
+            if (sec<10)
+            {
+                s = "0" + sec;
+            }
+            else
+            {
+                s = "" + sec;
+            }
+            return (m + ":" + s);
+        }
+        public void AddSec()
+        {
+            while (true)
+            {
+                this.time++;
+                Thread.Sleep(1000);
+            }
+        }
+
+
+    }
+}
---
+++ MainActivity.cs
@@ -0,0 +1,65 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System.Threading;
+using Android;
+using Android.Content.PM;
+using System;
+using Android.Support.V4.Content;
+
+namespace AvTipus2
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        TextView tvTime;
+        Exercise ex;
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.MasachOnGoing);
+            tvTime = (TextView)FindViewById(Resource.Id.tvTime);
+
+
+
+            Permission permission = ContextCompat.CheckSelfPermission(this, Manifest.Permission.AccessFineLocation);
+            if (permission == Permission.Granted)
+            {
+                ex = new Exercise();
+
+                ThreadStart threadTime = new ThreadStart(ex.AddSec);
+                Thread t1 = new Thread(threadTime);
+                t1.Start();
+
+                ThreadStart threadUpdate = new ThreadStart(UpdateTime);
+                Thread u1 = new Thread(threadUpdate);
+                u1.Start();
+            }
+            else
+                tvTime.Text = "11:11";
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        public void UpdateTime()
+        {
+            while (ex.isGoing)
+            {
+
+                RunOnUiThread(() =>
+                {
+                    tvTime.Text = ex.getTimer();
+                });
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
